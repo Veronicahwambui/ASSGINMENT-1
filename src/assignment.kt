b281b96ad59237eafb89219c6702525cba9f3ea7
veronicahwambui <njengaveronicahwambui@gmail.com>
2021-04-01 22:26:44 +0300

    first commit

---
+++ src/assignment.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val fullName:String=("veronicah")
+    println(fullName)
+    var age:Int=21
+    println(age)
+    var phoneNumber:String="0713156117"
+    println(phoneNumber)
+    var weightinkg:Double=50.7
+    println(weightinkg)
+    var citizen:Boolean=true
+    println(citizen)
+
+}
